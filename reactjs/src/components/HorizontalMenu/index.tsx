1f20c3313328b25d4e63acfaf823a4323a0151f5
poi <poi>
2019-11-06 19:05:05 +0800

    .

--- reactjs/src/components/HorizontalMenu/index.tsx
+++ reactjs/src/components/HorizontalMenu/index.tsx
@@ -1,57 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './index.scss';
 
-interface IState{
-    isScroll:boolean
-}
+export default () => {
+  const [hidden, sethidden] = useState(false);
 
-export default class index extends Component<{},IState> {
-  constructor(props: {}) {
-    super(props);
-    this.state={
-        isScroll:true
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return ()=>{
+      window.removeEventListener('scroll', handleScroll);
     }
-  }
+  }, []);
 
-  componentDidMount() {
-    //监听滚动隐藏导航事件
-    window.addEventListener('scroll', this.handleScroll);
-  }
-  componentWillUnmount() {
-    //卸载滚动吟唱导航事件
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-  //滚动隐藏导航使事件
-  handleScroll=(event: Event)=>{
+  let handleScroll = () => {
     let scrollTop = document.documentElement.scrollTop;
-    if (scrollTop === 0) {
-       this.setState({
-           isScroll:true
-       })
-    } else {
-        this.setState({
-            isScroll:false
-        })
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <header className={`${styles['poi-header']} ${this.state.isScroll?'poi-display-hidden':'poi-display-show'} noselect `}>
-          <div className={styles['poi-logo']} />
-          <div className={styles['poi-nav']}>
-            <ul>
-              <li>首页</li>
-              <li>首页</li>
-              <li>首页</li>
-              <li>首页</li>
-              <li>首页</li>
-            </ul>
-          </div>
-          <div className={styles['poi-other']} />
-        </header>
-      </div>
-    );
-  }
-}
+    if (scrollTop === 0) sethidden(true);
+    else sethidden(false);
+  };
+  return (
+    <div>
+      <header
+        className={`${styles['poi-header']} ${hidden ? 'poi-display-hidden' : 'poi-display-show'} noselect `}
+      >
+        <div className={styles['poi-logo']} />
+        <div className={styles['poi-nav']}>
+          <ul>
+            <li>首页</li>
+            <li>首页</li>
+            <li>首页</li>
+            <li>首页</li>
+            <li>首页</li>
+          </ul>
+        </div>
+        <div className={styles['poi-other']} />
+      </header>
+    </div>
+  );
+};
